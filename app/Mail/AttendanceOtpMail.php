bf2133226f7706d1bdd110b7c964eb4366c788dc
Junel Dolino <163298640+NelD0@users.noreply.github.com>
2025-09-21 15:29:27 +0800

    Create AttendanceOtpMail.php

---
+++ app/Mail/AttendanceOtpMail.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class AttendanceOtpMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public string $otp;
+
+    public function __construct(string $otp)
+    {
+        $this->otp = $otp;
+    }
+
+    public function build()
+    {
+        return $this->subject('Your Verification Code - MCC Payroll')
+            ->view('emails.attendance_otp');
+    }
+}
